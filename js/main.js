3723ba022f568b81b82a0e042ef2fa4ed5134733
medjedshin <yutabreaststroker0504@gmail.com>
2018-11-26 15:24:55 +0900

    Add reset function

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
   var btn = document.getElementById('btn');
   var triangle = document.getElementById('triangle');
   var output = document.getElementById('output');
+  var reset = document.getElementById('reset');
 
   name.addEventListener('click', function() {
     triangle.classList.remove('hidden');
@@ -23,6 +24,14 @@
     output.textContent = result.join('');
 
     // show
-    output.classList.remove('hidden');    
+    output.classList.remove('hidden');
+    reset.classList.remove('hidden');
+  });
+
+  reset.addEventListener('click', function() {
+    output.classList.add('hidden');
+    triangle.classList.add('hidden');
+    this.classList.add('hidden');
+    name.value = '';
   });
 })();
